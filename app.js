398eaa3de6653831b1b0585179811c009bca0b16
IonCatana <catana.ion17@yahoo.it>
2022-02-09 15:22:43 +0100

    Controllo del messagio true or false

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // Mail
 
 // Chiedi all'utente la sua email
-const email = prompt('Qualle é la tua email?')
-console.log(email)
+const email = prompt('Qualle é la tua email?');
+console.log(email);
 
 // Controlla che sia nella lista di chi puó accedere
 const listEmail = [
@@ -10,19 +10,22 @@ const listEmail = [
   'pippo2@gmail.com',
   'vscode.4@mail.ru',
   'slack.34@gmail.com',
-  'zoom.25@mail.ru'
-]
-console.log(listEmail)
+  'zoom.25@mail.ru',
+];
+console.log(listEmail);
 
 // Stampa un messagio apropriato sull'esito del controllo
-
-
-
-
-
-
-
-
+let check = false;
+for (let i = 0; i < listEmail.length; i++) {
+  console.log(listEmail[i]);
+  const element = listEmail[i];
+  if (element === email)
+    check = true;
+}
+if (check === true)
+  alert('Ben tornato');
+else
+  alert('Non sei abilitato');
 // Gioco dei Dadi
 
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer
