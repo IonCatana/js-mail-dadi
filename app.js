62e715808169bf3a9259721bc65ddbd289b74ca9
IonCatana <catana.ion17@yahoo.it>
2022-02-09 14:46:56 +0100

    Chiedi all'utente la sua Email

---
+++ app.js
@@ -0,0 +1,29 @@
+// Mail
+
+// Chiedi all'utente la sua email
+const email = prompt('Qualle é la tua email?')
+console.log(email)
+
+// Controlla che sia nella lista di chi puó accedere
+const listEmail = [
+  'catana.ion17@yahoo.it',
+  'pippo2@gmail.com',
+  'vscode.4@mail.ru',
+  'slack.34@gmail.com',
+  'zoom.25@mail.ru'
+]
+console.log(listEmail)
+
+// Stampa un messagio apropriato sull'esito del controllo
+
+
+
+
+
+
+
+
+// Gioco dei Dadi
+
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer
+// Stabilire il vincitore, in base a chi fa il punteggio piú alto
